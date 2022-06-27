362b25b0fc213ac5ac8edf14ea8b59c0b2964785
NeonMurder <105555625+NeonMurdered@users.noreply.github.com>
2022-06-27 14:22:12 +0300

    New Update
    Added :
    - Kills counter
    - Boss monsters (more 200 HP)

--- NeoCliker/assets/js/app.js
+++ NeoCliker/assets/js/app.js
@@ -6,13 +6,32 @@ document.addEventListener("DOMContentLoaded", function(event) {
    //---------------------------//
    let damage = 10; // damage inflicted on the monster
    //console log
-   var monsterHealth = hp;
+   let monsterHealth = hp;
+   let killedNumber = document.getElementById("killedNum").innerHTML = 0;
+   let monsterBoss = document.getElementById("boss").innerHTML;
    console.log("Monster Health : " + monsterHealth);
    //---------------------------//
    let hpTotal = hp;
    let numHP = document.getElementById('hp').innerHTML = hp + "/" + hpTotal;
    document.getElementById("hpProgress").value = hp;
    document.getElementById("hpProgress").max = hp;
+   searchBoss();
+   //Search to boss
+   function searchBoss() {
+      if(hp > 200)
+      {
+         monsterBoss = document.getElementById("boss").style.display = "block";
+         alert("Boss has been found!");
+         //console log
+         console.log("Boss has been found!");
+      }
+      else
+      {
+         monsterBoss = document.getElementById("boss").style.display = "none";
+         //console log
+         console.log("Boss not found!");
+      }
+   }
    //On Btn Attack click event
    document.getElementById('btnAttack').addEventListener("click", function(event){
       if(document.getElementById("hpProgress").value != 0)
@@ -27,9 +46,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
          hpRandom = hpPool[Math.floor(Math.random() * hpPool.length)];
          hp = hpRandom;
          monsterHealth = hp;
-         //console log
-         console.log("Monster Health : " + monsterHealth);
          hpTotal = hp;
+         killedNumber = document.getElementById("killedNum").innerHTML = killedNumber + 1;
          numHP = document.getElementById('hp').innerHTML = hp + "/" + hpTotal;
          document.getElementById("hpProgress").value = hp;
          document.getElementById("hpProgress").max = hp;
@@ -37,6 +55,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
          alert("Monster in Killed");
          //console log
          console.log("Monster in Killed");
+         console.log("Create a new Monster...");
+         console.log("Monster Health : " + monsterHealth);
+         searchBoss();
       }
    });
 });
